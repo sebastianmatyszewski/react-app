67623be41df5b66f7ef6f5bf0704fbde2c10857f
Sebastian Matyszewski <sebastianmat1@tlen.pl>
2021-12-13 12:13:59 +0100

    added the function of downloading quotes from the API, displaying a random quotation on the page, a button for randomizing the next quotation and a button to return to the previous quote

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      quotations: [],
+      quote: { quote: '', author: "" },
+      previousQuote: [],
+    };
+    this.getQuote= this.getQuote.bind(this);
+    this.getPreviousQuote= this.getPreviousQuote.bind(this);
+  }
 
+  getData = async () => {
+    try {
+      const data = await axios.get(
+        "https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json"
+      );
+      this.setState({quotations: data.data}); 
+    } catch (e) {
+      console.error(e);
+    }
+    this.getQuote();
+  };
+
+  getQuote(){
+    if(this.state.quote.quote !== "")
+      this.setState({previousQuote: this.state.previousQuote.concat(this.state.quote)});
+    this.setState({quote: this.state.quotations[Math.floor(Math.random()*this.state.quotations.length)]})
+  }
+
+   getPreviousQuote(){
+    this.setState({previousQuote: this.state.previousQuote.concat(this.state.quote)});
+    this.setState({quote: this.state.previousQuote[this.state.previousQuote.length-1]})
+    this.setState({previousQuote: this.state.previousQuote.filter(item => item !== this.state.previousQuote[this.state.previousQuote.length-1])});   
+  }
+
+  componentDidMount(){ 
+    this.getData(); 
+  }
+
+  render(){
+    return (
+      <div className="container">
+          <div className="content-container">
+            <div className="quote-container">
+              <div className="quote">
+              {this.state.quote.quote}
+              </div>
+              
+              <div className="author">
+                -{this.state.quote.author}
+              </div>
+            </div>
+            <div className="buttons-container">
+              <button onClick={this.getPreviousQuote} disabled={this.state.previousQuote.length===0}>Previous quote</button>
+              <button onClick={this.getQuote}>Generate next quote</button>
+            </div>
+          </div>
+      </div>
+    );
+  }
+};
 export default App;
